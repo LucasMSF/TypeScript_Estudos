0626d9af6188692e1305708fe3bbab9a92998937
LucasMSF <lucasgg003@gmail.com>
2022-06-08 01:14:18 -0300

    inicio do react com TS

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//EDU1 - Variáveis
+	const name: string = 'Lucas Mathias';
+	const age: number = 19;
+	const isWorking: boolean = true;
+
+	//EDU2 - Variáveis
+	function greeting(name: string) {
+		return `Olá ${name}, seja bem-vindo!`;
+	}
+
+	return (
+		<div className="App">
+			<h1>React Js com Typescript</h1>
+			<h3>Dev: {name}</h3>
+			<h4>Idade: {age}</h4>
+			<h4>Trabalhando atualmente? : {isWorking ? 'Sim' : 'Não'}</h4>
+			<br/>
+			<h1>{greeting(name)}</h1>
+
+		</div>
+	);
 }
 
 export default App;
